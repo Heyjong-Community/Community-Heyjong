e6b9fbb6ac4be9f3113a781175e39ec16bc83f8d
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-08-14 05:20:43 +0700

    add: layout login page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Barlow_Condensed } from 'next/font/google';
 import '../globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import Image from 'next/image';
 
 const barlowCondensed = Barlow_Condensed({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -33,8 +34,23 @@ export default function AuthLayout({
       <head>
         <link rel='icon' type='image/png' href='/images/logo-color.png' />
       </head>
-      <body className={`${barlowCondensed.className} antialiased`}>
+      <body className={`${barlowCondensed.className} relative antialiased`}>
+        <div className='absolute z-0 top-10 left-1/2 -translate-1/2 flex items-center'>
+          <Image
+            src='/images/logo-color.png'
+            width={300}
+            height={300}
+            alt='logo'
+            className='size-16 md:size-16 lg:size-20'
+          />
+          <h2 className='text-xl font-semibold text-white leading-5'>
+            HEYJONG <br /> COMMUNITY
+          </h2>
+        </div>
         {children}
+        <footer className='absolute z-0 bottom-5 text-white text-center font-medium left-1/2 -translate-1/2'>
+          &copy; 2025.Heyjong Community. All Rights Reserved
+        </footer>
         <Analytics />
       </body>
     </html>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,25 +1,60 @@
-// import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function LoginPage() {
   return (
     <div className='bg-[url("/images/jongcamp.webp")] h-screen bg-cover bg-center w-full'>
-      <div className='bg-black/30 h-full w-full flex items-center justify-center'>
-        {/* <div className='w-sm bg-white/30 backdrop-blur-sm h-80 rounded-lg p-7'>
-          <div className='flex items-center justify-center'>
-            <Image
-              src='/images/logo-color.png'
-              width={300}
-              height={300}
-              alt='logo'
-              className='size-16 md:size-16 lg:size-20'
-            />
-            <h2 className='text-xl text-white font-semibold leading-5'>
-              HEYJONG <br /> COMMUNITY
-            </h2>
+      <div className='bg-black/30 h-full w-full flex items-center justify-center px-5'>
+        <div className='w-sm bg-white/30 backdrop-blur-sm h-fit rounded-lg p-7'>
+          <h3 className='text-white text-3xl font-bold text-center uppercase'>Dashboard Jongers</h3>
+          <form className='space-y-5 mt-5'>
+            <div id='group-input' className='group'>
+              <label htmlFor='username' className='focus:text-primary text-white'>
+                Username
+              </label>
+              <input
+                type='text'
+                id='username'
+                name='username'
+                className='border-b-2 border-white w-full focus:border-primary text-white focus:duration-300 focus:outline-none py-2'
+              />
+            </div>
+            <div id='group-input'>
+              <label htmlFor='password' className='focus:text-primary text-white'>
+                Password
+              </label>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                className='border-b-2 border-white w-full focus:border-primary text-white focus:duration-300 focus:outline-none py-2'
+              />
+            </div>
+            <div className=''>
+              <button
+                disabled
+                className='w-full rounded-xl bg-primary/60 hover:bg-primary hover:duration-300 text-white font-semibold py-2 text-center text-base cursor-pointer'
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          <div className='mt-4'>
+            <p className='text-base text-white text-center'>Gak punya akun? atau gak bisa login?</p>
+            <p className='text-base text-white text-center'>
+              silakan chat{' '}
+              <Link
+                href={`#`}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='font-semibold underline decoration-primary underline-offset-2'
+              >
+                Admin
+              </Link>{' '}
+              ya
+            </p>
           </div>
-        </div> */}
-        <h1 className='text-white font-bold text-5xl text-center uppercase'>Coming Soon</h1>
+        </div>
       </div>
     </div>
   );
