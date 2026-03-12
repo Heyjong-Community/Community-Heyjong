084cae57ee83f2be7ff8be9109eac25f3b457ff2
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2026-03-12 11:07:51 +0700

    delete import mantine style

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,6 @@ import '../globals.css';
 import Sidebar from '@/components/organism/Sidebar';
 import NavDash from '@/components/organism/NavDash';
 import { Toaster } from 'sonner';
-import '@mantine/tiptap/styles.css';
 
 const barlowCondensed = Barlow_Condensed({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
