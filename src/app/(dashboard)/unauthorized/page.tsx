39896b8d91dd37985f9f9fde79cc1c0dda773a73
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2026-03-19 04:19:42 +0700

    fix: unauthorized page

---
+++ src/app/(dashboard)/unauthorized/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Unauthorized() {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <div className='text-center'>
+        <h2 className='text-black font-bold text-3xl'>Maaf 🙏</h2>
+        <p className='text-black font-medium'>Halaman ini tidak bisa diakses</p>
+      </div>
+    </div>
+  );
+}
